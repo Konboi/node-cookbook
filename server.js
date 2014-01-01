9872f8740b48bf5df15a440fffad41044640db70
Konboi <ryosuke.yabuki@gmail.com>
2014-01-01 04:51:21 +0000

    Added routing

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
 var http = require('http');
+var path = require('path');
+
+var pages = [
+  {route: '', output: 'Woohoo!!'},
+  {route: 'about', output: 'シンプルなサンプルこーどです'},
+  {route: 'another', output: function () {
+    return 'これが' + this.route;
+  }}
+];
 
 http.createServer(function (request, response) {
-    response.writeHead(200, {'Content-Type': 'text/html' });
-    response.end('Whoohoo!');
+    var lookup = path.basename(decodeURI(request.url));
+    pages.forEach(function (page) {
+        if (page.route === lookup) {
+          response.writeHead(200, {'Content-Type': 'text/html'});
+          response.end(typeof page.output === 'function' ? page.output() : page.output);
+        }
+    });
+    if (!response.finished) {
+      response.writeHead(404);
+      response.end('ページが見つかりません');
+    }
 }).listen(8080);
